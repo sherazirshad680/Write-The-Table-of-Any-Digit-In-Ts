f7d62cb02842088b036fa0dee131364772c0a25c
sherazirshad680 <134085129+sherazirshad680@users.noreply.github.com>
2024-05-10 00:32:12 +0500

    Write The Table of Any Digit.

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+var table = prompt(`Enter The Any Table`);
+for (var i = 1; i <= 10; i++) {
+    console.log(`${table} x ${i} = ${table * i}`);
+}
+// var table = prompt(`Enter The Any Table`);
+// var hello = prompt(`hello`);
+// for (var i = 1; i <= hello; i++) {
+//     console.log(`${table} x ${i} = ${table * i}`);
+// }
---
+++ index.ts
@@ -0,0 +1,10 @@
+var table: any = prompt(`Enter The Any Table`);
+for (var i = 1; i <= 10; i++) {
+  console.log(`${table} x ${i} = ${table * i}`);
+}
+
+// var table: any = prompt(`Enter The Any Table`);
+// var hello: any = prompt(`hello`);
+// for (var i = 1; i <= hello; i++) {
+//   console.log(`${table} x ${i} = ${table * i}`);
+// }
